dd6d916c44fbe58cc7222df2c9ab5ec5ce9787d1
CristianVlaz <75279611+CristianVlaz@users.noreply.github.com>
2020-12-09 17:47:42 -0300

    Delete ascii.cpp

--- ascii.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <stdio.h>
-char caracter;
-int main(){
-	printf("code_ascii\n");
-	printf("Ingresar un solo caracter: \n");
-	scanf("%c", &caracter);
-	printf("El codigo ASCII ingresado es: %i", caracter);
-	}
